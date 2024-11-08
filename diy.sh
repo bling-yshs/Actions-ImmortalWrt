0206b7b5b9e1be96abbc0409d60c0ee4ecc963ad
bbvdf <bbvdf@users.noreply.github.com>
2024-11-09 02:04:51 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -2,8 +2,3 @@
 # Please enter the custom command below (usually used to install third-party plugins) (can be left blank)
 # git clone --depth=1 https://github.com/EOYOHOO/UA2F.git package/UA2F
 # git clone --depth=1 https://github.com/EOYOHOO/rkp-ipid.git package/rkp-ipid
-git clone https://github.com/izilzty/luci-app-chinadns-ng.git package/luci-app-chinadns-ng
-echo >> feeds.conf.default
-echo 'src-git istore https://github.com/linkease/istore;main' >> feeds.conf.default
-./scripts/feeds update istore
-./scripts/feeds install -d y -p istore luci-app-store
